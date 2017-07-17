5947cd58e3e15304232faefacea317e952163261
baishaohua <shaohua.bai@dianjoy.com>
2017-07-17 15:38:48 +0800

    提交变更文件aliyun_slb,负载均衡sdk

---
+++ test/cmd_online.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+
+__created__ =  2017/6/29 14:55
+__author__ = 'baishaohua'
+# @Site    : https://github.com/bashhu
+"""
+
---
+++ utils/aliyun_slb.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+
+__created__ =  2017/7/1 23:00
+__author__ = 'baishaohua'
+# @Site    : https://github.com/bashhu
+"""
+
